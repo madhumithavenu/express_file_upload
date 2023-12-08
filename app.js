05211adb8634a0e94dce11c385dfbd6a184413e2
Madhumitha <madhumithavenu03@gmail.com>
2023-12-08 10:48:00 +0530

    uploading the image from browser

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
+const fileupload = require('express-fileupload');
 
 const app = express();
 
-app.engine('hbs', exphbs({ extname: '.hbs'}));
+app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
-app.listen(5000, ()=>{
+app.use(fileupload());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/store', (req, res) => {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('no files were uploaded')
+    }
+
+    sampleFile = req.files.sampleFile;
+    console.log(sampleFile);
+});
+
+
+app.listen(5000, () => {
     console.log('Listening to port number 5000');
 });
