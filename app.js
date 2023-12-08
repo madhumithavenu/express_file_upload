0d318f024961c1082f326a9540a3d4cc7ef97f1b
Madhumitha <madhumithavenu03@gmail.com>
2023-12-08 10:12:51 +0530

    setting up express-handlebars hbs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('hbs', exphbs({ extname: '.hbs'}));
+app.set('view engine', 'hbs');
 
 app.listen(5000, ()=>{
     console.log('Listening to port number 5000');
